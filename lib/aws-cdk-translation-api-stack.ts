193a29dc00fec33764bd03efe1de2e05fcbda33c
unknown <2867459421@qq.com>
2025-03-22 11:00:19 +0800

    verified GET and POST /things endpoints with Postman

--- lib/aws-cdk-translation-api-stack.ts
+++ lib/aws-cdk-translation-api-stack.ts
@@ -1,63 +1,60 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'; 
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// Lambda
-import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+
 // API Gateway
-import { RestApi, LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+
 export class AwsCdkTranslationApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    
+    // Create a DynamoDB table
     const table = new Table(this, 'MyApiTable', {
       tableName: 'MyApiTable',
-      partitionKey: {
-        name: 'pk',
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: 'sk',
-        type: AttributeType.STRING,
-      },
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      sortKey: { name: 'sk', type: AttributeType.STRING },
       billingMode: BillingMode.PAY_PER_REQUEST,
     });
 
-    
     new CfnOutput(this, 'TableNameOutput', {
-      value: table.tableName
-    });
-     
-     const getAllItemsFunction = new Function(this, 'GetAllItemsFunction', {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset('lambdas'),       
-      handler: 'getAllItems.handler',        
-      environment: {
-        TABLE_NAME: table.tableName,         
-      },
+      value: table.tableName,
     });
 
-    table.grantReadWriteData(getAllItemsFunction);
-
-    const createItemFunction = new Function(this, 'CreateItemFunction', {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset('lambdas'),
-      handler: 'createItem.handler',
+    //Use Nord to create Getalitensen
+    const getAllItemsFunction = new NodejsFunction(this, 'GetAllItemsFunction', {
+      runtime: Runtime.NODEJS_18_X,
+      entry: join(__dirname, '..', 'lambdas', 'getAllItems.ts'),
       environment: {
         TABLE_NAME: table.tableName,
       },
     });
+    table.grantReadWriteData(getAllItemsFunction);
 
+    //Use Nord to create a crititten finchton
+    const createItemFunction = new NodejsFunction(this, 'CreateItemFunction', {
+      runtime: Runtime.NODEJS_18_X,
+      entry: join(__dirname, '..', 'lambdas', 'createItem.ts'),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
     table.grantReadWriteData(createItemFunction);
 
-     //Create and configure an API Gateway
-     const api = new RestApi(this, "AppApi", {
-      restApiName: "ThingsService",
+    //Create and configure an API Gateway
+    const api = new RestApi(this, 'AppApi', {
+      restApiName: 'ThingsService',
     });
 
-    const things = api.root.addResource("things");
+    const things = api.root.addResource('things');
 
-    things.addMethod("GET", new LambdaIntegration(getAllItemsFunction));
-    things.addMethod("POST", new LambdaIntegration(createItemFunction));
+    // GET /things
+    things.addMethod('GET', new LambdaIntegration(getAllItemsFunction));
+    // POST /things
+    things.addMethod('POST', new LambdaIntegration(createItemFunction));
   }
 }
